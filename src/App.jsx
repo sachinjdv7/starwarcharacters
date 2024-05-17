38381192aeab27d46e0a5495b5a224c818e2bec4
sachinjadhav <sachinjdv7@gmail.com>
2024-05-17 18:23:34 +0530

    basic code cleanup and chakra ui setup complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Heading } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Box textAlign="center">
+        <Heading>I'm a Heading</Heading>
+      </Box>
+    </>
+  );
+}
+
+export default App;
